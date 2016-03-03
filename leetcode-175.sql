5b361171f093c87c71b928909f091a0df727e7d5
Vincent Sanchez <vincent3426@yeah.net>
2016-03-03 20:29:27 +0800

    Combine Two Tables

---
+++ leetcode-175.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT FirstName, LastName, City, State FROM Person LEFT JOIN Address USING(PersonId);
+
+SELECT FirstName, LastName, City, State FROM Person P LEFT JOIN Address A ON P.PersonId = A.PersonId
